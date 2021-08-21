dd94ca48ccbb76a929262e96af8cc75a486db24e
NanetteXD <89321391+NanetteXD@users.noreply.github.com>
2021-08-21 22:09:07 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+#
+# NitroGen
+#  
+# This work is licensed under GNU
+# GENERAL PUBLIC LICENSE
+#
+
+import random, string
+import os
+
+reset = "\e[0m"
+red = "\e[0;31m"
+green = "\e[0;32m"
+
+try:
+    os.remove("nitroCodes.txt")
+except Exception as e:
+    print("Error " + str(e))
+
+code = "https://discord.gift/"
+code += ('').join(random.choices(string.ascii_letters + string.digits, k=16))
+
+f = open("nitroCode.txt", "a+")
+f.write(f"{code}\n")
+f.close()
+print(f"{code}")
